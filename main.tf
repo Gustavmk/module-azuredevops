96eae43283b5902b80f5f975103284215464e9de
Kuno <gustavo.kuno@linx.com.br>
2023-01-22 19:44:11 -0300

    .

--- main.tf
+++ main.tf
@@ -1,89 +1,37 @@
-variable "proj_name" {
-  description = "Azure DevOps Project name."
-  type        = string
+# create project
+resource "azuredevops_project" "proj" {
+  name               = var.proj_name
+  description        = var.proj_description
+  visibility         = var.proj_visibility
+  version_control    = var.proj_vcs
+  work_item_template = var.proj_wi_template
+  features           = var.proj_features
 }
 
-variable "proj_description" {
-  description = "Azure DevOps Project description."
-  type        = string
-}
+# create github service connection
+resource "azuredevops_serviceendpoint_github" "gh" {
+  project_id            = azuredevops_project.proj.id
+  service_endpoint_name = var.github_serviceendpoint_name
+  description           = var.github_serviceendpoint_description
 
-variable "proj_visibility" {
-  description = "Project visibility."
-  type        = string
-  default     = "private"
-}
-
-variable "proj_vcs" {
-  description = "Project Version Control."
-  type        = string
-  default     = "Git"
-}
-
-variable "proj_wi_template" {
-  description = "Project Work Item template."
-  type        = string
-  default     = "Agile"
-}
-
-variable "proj_features" {
-  description = "Project features."
-  type        = map(string)
-  default = {
-    "boards"       = "disabled"
-    "repositories" = "enabled"
-    "pipelines"    = "enabled"
-    "testplans"    = "disabled"
-    "artifacts"    = "disabled"
+  auth_personal {
+    personal_access_token = var.github_pat
   }
 }
 
-variable "github_serviceendpoint_name" {
-  description = "GitHub Service Endpoint name."
-  type        = string
-}
+# create variable group
+resource "azuredevops_variable_group" "vg" {
+  project_id   = azuredevops_project.proj.id
+  name         = var.vg_name
+  description  = var.vg_description
+  allow_access = var.vg_allow_access
 
-variable "github_serviceendpoint_description" {
-  description = "GitHub Service Endpoint description."
-  type        = string
-  default     = "Managed by Terraform"
-}
-
-variable "github_pat" {
-  description = "The Personal Access Token for Github."
-  type        = string
-}
-
-variable "vg_name" {
-  description = "The name of the Variable Group."
-  type        = string
-}
-
-variable "vg_description" {
-  description = "The description of the Variable Group."
-  type        = string
-}
-
-variable "vg_allow_access" {
-  description = "Boolean that indicate if this variable group is shared by all pipelines of this project."
-  type        = bool
-  default     = false
-}
-
-variable "vg_variables" {
-  type = list(object({
-    name      = string
-    value     = string
-    is_secret = bool
-  }))
-  description = <<EOD
-One or more variable blocks as documented below.
-
-A variable block supports the following:
-
-  * name - (Required) The key value used for the variable. Must be unique within the Variable Group.
-  * value - (Optional) The value of the variable. If omitted, it will default to empty string.
-  * is_secret - (Optional) A boolean flag describing if the variable value is sensitive. Defaults to false.
-EOD
-  default     = []
+  dynamic "variable" {
+    for_each = var.vg_variables
+    content {
+      name      = variable.value["name"]
+      value     = variable.value["value"]
+      is_secret = variable.value["is_secret"]
+    }
+  }
 }
